5dc137d318652f49370b7f90a6f1e6b8bd74b52f
Thomas Abraham <thomasabraham832@gmail.com>
2022-07-05 02:59:12 +0100

    second commit

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/ping")
 	public String index() {
 		return "pong";
 	}
